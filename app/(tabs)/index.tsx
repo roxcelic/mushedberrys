ecb19ed5391795fd4274490c476f3b66035b4f32
mushy <157179472+roxcelic@users.noreply.github.com>
2025-06-10 14:31:42 +0100

    I AM LEARNING JAVASCRIPT

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,37 +1,74 @@
-import { StyleSheet } from 'react-native';
-
 import Main from '@/components/Main';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 import { Image } from 'expo-image';
+import React, { Component } from 'react';
+import { StyleSheet } from 'react-native';
 
-export default function HomeScreen() {
-  return (
-    <Main
-        headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      >
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type='title'>
-          Hey
-        </ThemedText>
-      </ThemedView>
-      <ThemedView>
+
+export default class HomeScreen extends Component {
+  run = () => {
+      setInterval(() => {
+        this.setState({number: this.state.number + 1});
+      }, 1000);
+}
+
+
+   constructor() {
+      super();
+      this.state = {
+         myText: '1',
+         number: 0
+      }
+      this.run();
+   }
+
+  updateText = () => {
+    switch (this.state.myText) {
+      case '1': 
+        this.setState({myText: '2'});
+
+        break;
+      case '2': 
+        this.setState({myText: '1'});
+
+        break;
+    }
+  }
+   
+  render() {
+    return (
+      <Main
+          headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
+        >
+        <ThemedView style={styles.titleContainer}>
+          <ThemedText type='title'>
+            Hey
+          </ThemedText>
+        </ThemedView>
+        <ThemedView>
+          <ThemedText>
+            Hiiiiiii, this is really really really stupid lmaos
+          </ThemedText>
+          <Image
+          style={styles.tinyLogo}
+          source={{
+            uri: 'https://i.pinimg.com/736x/de/f4/55/def455a0b9b70d101e71429e1fb092fa.jpg',
+          }}
+        />
         <ThemedText>
-          Hiiiiiii, this is really really really stupid lmaos
+          hehehehe i added an image frick you
         </ThemedText>
-        <Image
-        style={styles.tinyLogo}
-        source={{
-          uri: 'https://i.pinimg.com/736x/de/f4/55/def455a0b9b70d101e71429e1fb092fa.jpg',
-        }}
-      />
-      <ThemedText>
-        hehehehe i added an image frick you
-      </ThemedText>
-      </ThemedView>
-    
-    </Main>
-  );
+        </ThemedView>
+        <ThemedView>
+          <ThemedText onPress = {this.updateText}>
+            {this.state.number}
+            </ThemedText>
+        </ThemedView>
+      
+      </Main>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
